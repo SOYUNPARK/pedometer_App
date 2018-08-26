720e182cd104cbf3f59f053288dce98d651daaf0
SOYUN PARK <yeonsozzz@gmail.com>
2018-08-26 13:57:31 +0900

    created new project

---
+++ app/src/main/java/kr/ac/gachon/mmmi/pedometer/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.gachon.mmmi.pedometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
